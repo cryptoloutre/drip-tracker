6172a5251d288ee33c338fd4928ad01cbbf13a39
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-07-11 09:00:07 +0200

    add drop13 degen poet

---
+++ src/views/degenpoet/drop13/NFTsinDrop.tsx
@@ -0,0 +1,21 @@
+export const NFTsinDrop = [
+  {
+    name: "Fireworks",
+    rarity: "common",
+    image: "https://arweave.net/SS80YVoWJpR_Jl62lSzuTJjHan-vBn2SpPbLS9gZnh4?ext=jpg",
+    magicEdenLink: "https://www.tensor.trade/trade/drip_degen_poet",
+  },
+  {
+    name: "Type Saturn",
+    rarity: "rare",
+    image: "https://arweave.net/djWGXJM-Z6bZmc3GPKUiu_J0VhzYbLRFW0jKJXYe1JE?ext=jpg",
+    magicEdenLink: "https://www.tensor.trade/trade/drip_degen_poet",
+  },
+  {
+    name: "Water Lily",
+    rarity: "legendary",
+    image: "https://arweave.net/VDGYVfvCl-ARU1RdPyu9BbSbn3sSeohP0JA432SOnEw?ext=jpg",
+    magicEdenLink: "https://www.tensor.trade/trade/drip_degen_poet",
+  },
+];
+
--- src/views/degenpoet/home/index.tsx
+++ src/views/degenpoet/home/index.tsx
@@ -454,6 +454,19 @@ export const DegenHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">DROP 12</div>
               </Link>
+              <Link
+                href="/degenpoet/drop13"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/VDGYVfvCl-ARU1RdPyu9BbSbn3sSeohP0JA432SOnEw?ext=jpg"
+                    alt="drop 13 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">DROP 13</div>
+              </Link>
             </div>
           </div>
         </div>
