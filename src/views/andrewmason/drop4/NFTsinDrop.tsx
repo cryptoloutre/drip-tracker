95f8b770a99d7064487fa1a8500f3f677f0ad64e
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-07-31 10:09:30 +0200

    add drop4 andrew mason

---
+++ src/views/andrewmason/drop4/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'Infinity',
+    uri: 'https://arweave.net/xq-cTqLHh9l7vAcIYslps1AFx7-aKuS1XJfS7KffYcE',
+    rarity: 'Common',
+    image: 'https://arweave.net/z_95HU2wdlXW7SaBUlLOeQ83w50scgD2gi6LTJBhOLc?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_andrewmason_season_1'
+  },
+  {
+    name: 'Infinity',
+    uri: 'https://arweave.net/W1yrX8u164Zq1E4fhMAfml6aptPst2Q_FFqch9ZU2Rc',
+    rarity: 'Rare',
+    image: 'https://arweave.net/E5FMPktyCPuVdtQbCzf1LCLPL0OfV_Nzpj1KVdc0m54?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_andrewmason_season_1'
+  },
+  {
+    name: 'Infinity',
+    uri: 'https://arweave.net/Kduiwng9GJcYCu1aYIssdsvZSCT61TlG8pnaR9l7JSs',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/w-xzG-gCfigM7y3KItrBUNit1VFakudDi-mtXn5lWj4?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_andrewmason_season_1'
+  }
+];
--- src/views/andrewmason/home/index.tsx
+++ src/views/andrewmason/home/index.tsx
@@ -309,6 +309,19 @@ export const MasonHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 3</div>
               </Link>
+              <Link
+                href="/andrewmason/drop4"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/w-xzG-gCfigM7y3KItrBUNit1VFakudDi-mtXn5lWj4?ext=jpg"
+                    alt="drop 4 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
+              </Link>
             </div>
           </div>
         </div>
