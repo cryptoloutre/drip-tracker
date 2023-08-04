625f788898f4620657660e35ec1657af270384c9
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-04 09:18:03 +0200

    add drop5 maquin

---
+++ src/views/maquin/drop5/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'Focused Mind',
+    uri: 'https://arweave.net/rvfZaC_bhzvDSZ0X_1T7jTU8Tblse0AGiRJiTOe49kc',
+    rarity: 'Common',
+    image: 'https://arweave.net/ESRDs49il1ldLgWJDLCptIil02i3hsayoQtbJ4yXqlM?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Fevered Mind',
+    uri: 'https://arweave.net/zTYfpp3m5PVqEPl0gkuOLgke7RdkCSHOMjO4JSPi8qc',
+    rarity: 'Rare',
+    image: 'https://arweave.net/d2XvhEda5qAdmtC4oEwvrnMNTqu8jP6UuC6AHxCo0As?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Passionate Mind',
+    uri: 'https://arweave.net/Qq7gTHzp8MMxosEt8OtOcJKM_anZD4pPM3eKAq1Kqj8',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/EVyaz3sz3aFRBP72yjvks0wOGYEDIDupUMVMoKewioA?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+];
--- src/views/maquin/home/index.tsx
+++ src/views/maquin/home/index.tsx
@@ -319,6 +319,19 @@ export const MaquinHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
               </Link>
+              <Link
+                href="/maquin/drop5"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/EVyaz3sz3aFRBP72yjvks0wOGYEDIDupUMVMoKewioA?ext=jpg"
+                    alt="drop 5 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 5</div>
+              </Link>
             </div>
           </div>
         </div>
