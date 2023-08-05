dae2299aa0360c05dc7374df09e192a7ea91717c
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-05 15:20:06 +0200

    add drop7 tiny

---
+++ src/views/tinydenise/drop7/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'Honey',
+    uri: 'https://arweave.net/Iz9rZSmEVDGRJ3K0MOgLqGy_FwPHVOhwMIljONi4o4o',
+    rarity: 'Common',
+    image: 'https://arweave.net/5teARWvs8Tlx8_mv_CYWlXLr5RMyxbp6QGu50NT2bNw?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_tiiiny_stamps'
+  },
+  {
+    name: 'Bee',
+    uri: 'https://arweave.net/rU9gbkOLxnAjKWZllLAGstNyrL14o3DLm-eeLoP7yWY',
+    rarity: 'Rare',
+    image: 'https://arweave.net/sH8GrChLbRh52llhPw4JX_jUFJmxWphZFFXYs_c-aAE?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_tiiiny_stamps'
+  },
+  {
+    name: 'Let’s Bee Kind',
+    uri: 'https://arweave.net/JpaWkpXXoJtgV1oANJkgWJzlkWtQ1g0UdqwRw8W5FDQ',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/FZh1ts2JBEcR-tCPSwWsj8FOR-rigHhnjSgyhTXyX3M?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_tiiiny_stamps'
+  },
+];
