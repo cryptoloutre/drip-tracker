2424799630a1317e60f4964ae8a8226d817d393d
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-05 15:19:43 +0200

    add drop3 0xStoek

---
+++ src/views/0xStoek/drop3/NFTsinDrop.tsx
@@ -0,0 +1,9 @@
+export const NFTsinDrop = [
+  {
+    name: 'Let It Kill You',
+    uri: 'https://arweave.net/oVh92i19tfq_G8CU9DgjKWT8ykobobmbvI2SlGqQf7I',
+    rarity: 'Common',
+    image: 'https://arweave.net/37GSIybJB60V1RrhmCrJ8VTnYZqSUA-80SHA21STZK4?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_0xStoek_season_0'
+  }
+];
--- src/views/0xStoek/home/index.tsx
+++ src/views/0xStoek/home/index.tsx
@@ -299,6 +299,19 @@ export const StoekHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 2</div>
               </Link>
+              <Link
+                href="/0xStoek/drop3"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/37GSIybJB60V1RrhmCrJ8VTnYZqSUA-80SHA21STZK4?ext=png"
+                    alt="drop 3 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 3</div>
+              </Link>
             </div>
           </div>
         </div>
