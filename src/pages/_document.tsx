6ed4dee3605df0174f71f89fa3127d6f8c2bfe63
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-20 18:32:48 +0200

    change favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,28 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+        <meta
+            name="description"
+            content="Track the DRiP.haus NFTs you are missing"
+          />
         <link rel="shortcut icon" href="/favicon.ico"/>
+        <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
         </Head>
         <body>
           <Main />
