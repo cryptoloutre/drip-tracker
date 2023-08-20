eec7d36304b11129fe656868cbc69aba1173f79f
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-20 10:23:47 +0200

    add jakey drop

---
+++ src/views/jakey/drop3/NFTsinDrop.tsx
@@ -0,0 +1,9 @@
+export const NFTsinDrop = [
+  {
+    name: 'Soccer wax roulette',
+    uri: 'https://arweave.net/A0Ph0HnHywkxly09klvIvpcOa99xhGkeUDhYw8ztW6I',
+    rarity: "Common",
+    image: 'https://arweave.net/e_rFJkB9QW-0oLxGEJFQamG7kwGfRS5-BEfyXkrkzHA?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_jakey_season_1'
+  }
+];
--- src/views/jakey/home/index.tsx
+++ src/views/jakey/home/index.tsx
@@ -296,6 +296,19 @@ export const JakeyHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 2</div>
               </Link>
+              <Link
+                href="/jakey/drop3"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/e_rFJkB9QW-0oLxGEJFQamG7kwGfRS5-BEfyXkrkzHA?ext=png"
+                    alt="drop 3 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 3</div>
+              </Link>
             </div>
           </div>
         </div>
