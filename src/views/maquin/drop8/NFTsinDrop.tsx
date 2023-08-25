8e1f89c473763ab48781e151aaddd78cee6458e3
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-25 08:35:23 +0200

    add drop8 maquin

---
+++ src/views/maquin/drop8/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'Metal Eggs',
+    uri: 'https://arweave.net/9MUJKJ67nAnKt9Q0nqGZvdGLXTETSlTKbosCerYqnLA',
+    rarity: 'Common',
+    image: 'https://arweave.net/1MFsjKVfdx8IwozrRHlH0Uz2YncfRRdn2GN7LQNiCOg?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Alien Eggs',
+    uri: 'https://arweave.net/9JxDk8HVK6WJsvR7JytINl0fcj-uGwXFq4zT4DO8buc',
+    rarity: 'Rare',
+    image: 'https://arweave.net/x0RgOnYH3VJz-zR5EdXEgvN53259GogPgy-1e3Bopeo?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Dragon Eggs',
+    uri: 'https://arweave.net/ixYP2qe5Jc4Dmr32JOyu8B-YwHLk6MXBHe5T-b56zjw',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/4-2hlh1SwmGX8m9Qf4IQHN8WUCd3jU6cuyq11FUPuTg?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+];
--- src/views/maquin/home/index.tsx
+++ src/views/maquin/home/index.tsx
@@ -358,6 +358,19 @@ export const MaquinHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 7</div>
               </Link>
+              <Link
+                href="/maquin/drop8"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/4-2hlh1SwmGX8m9Qf4IQHN8WUCd3jU6cuyq11FUPuTg?ext=jpg"
+                    alt="drop 8 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 8</div>
+              </Link>
             </div>
           </div>
         </div>
