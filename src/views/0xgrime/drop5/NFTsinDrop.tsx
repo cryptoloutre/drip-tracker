9c3e625a9aa528a29f8607ae9cf64fc62b7bbc67
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-08-28 09:28:57 +0200

    add drop5 0xgrime

---
+++ src/views/0xgrime/drop5/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'De',
+    uri: 'https://arweave.net/JvxsQsdqNRnDUWCT7ANgYN-iZEM4Bq3Q6f3KNbhq-l8',
+    rarity: 'Common',
+    image: 'https://arweave.net/DikexL1riLK2SQmbufDu574g21EcP6Kw2nCkgpiHO5Y?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_0xgrime_season_1'
+  },
+  {
+    name: 'DeRare',
+    uri: 'https://arweave.net/Fzax8YftIVKg6-EV5QacgX6IjRYhCwzBwJeWt4Zy5iU',
+    rarity: 'Rare',
+    image: 'https://arweave.net/wKtBPleAnnNA7O7eedbnnmLGINzA6VmbO2bpbmJa4N4?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_0xgrime_season_1'
+  },
+  {
+    name: 'DeLegendary',
+    uri: 'https://arweave.net/UhtcP5KrhJ4MGXBiwLqQlzJCKgTfktIcMwWYA1pHOiQ',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/AGzyWPK2pRqqDD_SXe-9NBEuyK3OxcYA3NrJ43LT0eo?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_0xgrime_season_1'
+  },
+];
--- src/views/0xgrime/home/index.tsx
+++ src/views/0xgrime/home/index.tsx
@@ -321,6 +321,19 @@ export const GrimeHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
               </Link>
+              <Link
+                href="/0xgrime/drop5"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/AGzyWPK2pRqqDD_SXe-9NBEuyK3OxcYA3NrJ43LT0eo?ext=jpg"
+                    alt="drop 5 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 5</div>
+              </Link>
             </div>
           </div>
         </div>
