4eb7746b1135eaac296919f377f43dd6c3e95fef
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-09-08 08:26:46 +0200

    add maquin drop10

---
+++ src/views/maquin/drop10/NFTsinDrop.tsx
@@ -0,0 +1,23 @@
+export const NFTsinDrop = [
+  {
+    name: 'Petal',
+    uri: 'https://arweave.net/aCBIDhZNF0PkrIE-GFsoLUhlf1DIKTVhsaijTRmIaho',
+    rarity: 'Common',
+    image: 'https://arweave.net/8e2F4r4c8Q8ENKwX_ty0RrKG6gAaAfXiAXZ9B-Qo6Lo?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Frost Petal',
+    uri: 'https://arweave.net/2IlbJ-TrFXef-dN-37xRyXZzoyfUQJYrN2QuDnqEoGA',
+    rarity: 'Rare',
+    image: 'https://arweave.net/7W4I8AGJYTJ1paiH21xKt2fhYSfUS0DVXLRtmSoYhrI?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Sapphire Petal',
+    uri: 'https://arweave.net/ZoTHSH7n9381GO-lrpKA04Bm--RU2N4pfzRmWRzedyI',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/ZiuetcQiVrKxxvr0aHsqURPqbQ3mENzvk3VEKhBNiEQ?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+];
--- src/views/maquin/home/index.tsx
+++ src/views/maquin/home/index.tsx
@@ -384,6 +384,19 @@ export const MaquinHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 9</div>
               </Link>
+              <Link
+                href="/maquin/drop10"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/ZiuetcQiVrKxxvr0aHsqURPqbQ3mENzvk3VEKhBNiEQ?ext=jpg"
+                    alt="drop 10 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 10</div>
+              </Link>
             </div>
           </div>
         </div>
