0f2dcaae7300bee1b4ed9b81bbd9ef178b8f5280
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-09-23 14:31:56 +0200

    add drop7 0xgrime

---
+++ src/views/0xgrime/drop7/NFTsinDrop.tsx
@@ -0,0 +1,9 @@
+export const NFTsinDrop = [
+  {
+    name: 'AWESOME!',
+    uri: 'https://arweave.net/f7cTkZQc4T5CelfHDw7WgnfFU6e5dxwQHQEFNJIpKec',
+    rarity: 'Common',
+    image: 'https://arweave.net/Bo2xv5kS_2AA9cb7V_qMDRYbDgDEXtAERGlCINmqBXE?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_0xgrime_season_1'
+  },
+];
