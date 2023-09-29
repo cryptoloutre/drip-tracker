eb7665b211e04ddf3e8a6d6e5b1e2d35ce1f0955
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-09-29 10:57:33 +0200

    add drop13 maquin

---
+++ src/views/maquin/drop13/NFTsinDrop.tsx
@@ -0,0 +1,30 @@
+export const NFTsinDrop = [
+  {
+    name: 'Broken',
+    uri: 'https://arweave.net/mYlvL6gXz66xHnP6HovObevVx3JSJ-poVPv024_XrJA',
+    rarity: 'Common',
+    image: 'https://arweave.net/i_uNeBnpBgpz5ZDkutcWdg0PomIBO0W6iEuddTgoGjI?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Broken Stars',
+    uri: 'https://arweave.net/KZwPJwYtpK_aMP-9QviEyzqJtqH3v-RUoHznX-XpKko',
+    rarity: 'Rare',
+    image: 'https://arweave.net/wABFPAryXNCWqj8f5AKVbBJbZJ2lyXIiLZHK3Xt0uE8?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Broken Galaxies',
+    uri: 'https://arweave.net/f8i4gAHK-T_LkjOdSIIO0Ro_ViiqtzUNUOhfGuOBZPQ',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/2I4KpaI8OSHtdwaT4ZPPIeUppAXUQc_L_twLKYkCvro?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Broken Cosmos',
+    uri: '',
+    rarity: 'Ultimate',
+    image: 'https://arweave.net/iYhuEK_MbQAcRjYVMVPN6NYh8OJJ9MtR-w9oUcUOQo4?ext=jpg',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+];
