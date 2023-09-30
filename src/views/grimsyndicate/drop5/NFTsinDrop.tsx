f871ec4c820fc14acb0b2e0412937f1a7e6130ef
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-09-30 10:16:08 +0200

    add drop 5 grim syndicate

---
+++ src/views/grimsyndicate/drop5/NFTsinDrop.tsx
@@ -0,0 +1,17 @@
+export const NFTsinDrop = [
+  {
+    name: "The Price of Progress",
+    uri: "https://arweave.net/WUNSJI9LcFvKPENb6pfynXHiU4f1072tUJ2b7IPhkDA",
+    rarity: "Common",
+    image: "https://arweave.net/9xcdeB17ZcB2GguhW4NcBq8gB0-k-74QyP9mCXRp2ow?ext=png",
+    magicEdenLink: "https://www.tensor.trade/trade/drip_grim_lore",
+  },
+  {
+    name: "The Price of Progress",
+    uri: "https://arweave.net/UGk0idbNF7kepQDd7-8QhiWr8FU8umBwK_jkscvg8CA",
+    rarity: "Rare",
+    image: "https://arweave.net/OuQt6GXpnlhDzmE-4BZiufNSGSRo-PLXJ801xd3SGHo?ext=jpg",
+    magicEdenLink: "https://www.tensor.trade/trade/drip_grim_lore",
+  }
+];
+
