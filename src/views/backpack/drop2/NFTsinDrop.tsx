531cf4d6cc3a85089fd70ae9f69832afd83ad7b2
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-10-03 11:12:35 +0200

    add drop2 backpack

--- src/views/backpack/drop2/NFTsinDrop.tsx
+++ src/views/backpack/drop2/NFTsinDrop.tsx
@@ -1,29 +1,5 @@
 export const NFTsinDrop = [
   {
-    name: "Left Leg of the Mad One",
-    uri: "https://arweave.net/fNOZ5TK7fWVgREAR47Lxrwt0-OwQxPBP4yFK73E45TI",
-    rarity: "Common",
-    image:
-      "https://arweave.net/u9jMrb_iyiN41w3Sb6m2Y_4KPKqyPQJdLfWt-fAghQQ?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
-  },
-  {
-    name: "Right Leg of the Mad One",
-    uri: "https://arweave.net/anGE0Rl9ZR-221pQb94d3ZXadC-arS8JkHPNVboUpGc",
-    rarity: "Common",
-    image:
-      "https://arweave.net/7RouNh6-i7H9XgJtA-3igeLxy3KRcpzq_FAA1cgYPDc?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
-  },
-  {
-    name: "Left Arm of the Mad One",
-    uri: "https://arweave.net/_lzWqlUSbyGgoASkhSiqhE1rBH_Hmyw67EBfW6pKwgc",
-    rarity: "Rare",
-    image:
-      "https://arweave.net/KNuUWKaVFkPAp8sgObu2kfsO1WB7CQ8hNJkzADhMn1c?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
-  },
-  {
     name: "Right Arm of the Mad One",
     uri: "https://arweave.net/jer53t7YTu3WEqnmWJFJ6zETPExGqr5nSeKocanxcz4",
     rarity: "Legendary",
@@ -39,4 +15,29 @@ export const NFTsinDrop = [
       "https://arweave.net/vJZloGItHaFFUNguWg6nOr-LV8ycwkIf6AejWw__5z0?ext=png",
     magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
   },
+  {
+    name: "Left Arm of the Mad One",
+    uri: "https://arweave.net/_lzWqlUSbyGgoASkhSiqhE1rBH_Hmyw67EBfW6pKwgc",
+    rarity: "Rare",
+    image:
+      "https://arweave.net/KNuUWKaVFkPAp8sgObu2kfsO1WB7CQ8hNJkzADhMn1c?ext=png",
+    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
+  },
+  {
+    name: "Right Leg of the Mad One",
+    uri: "https://arweave.net/anGE0Rl9ZR-221pQb94d3ZXadC-arS8JkHPNVboUpGc",
+    rarity: "Common",
+    image:
+      "https://arweave.net/7RouNh6-i7H9XgJtA-3igeLxy3KRcpzq_FAA1cgYPDc?ext=png",
+    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
+  },
+  {
+    name: "Left Leg of the Mad One",
+    uri: "https://arweave.net/fNOZ5TK7fWVgREAR47Lxrwt0-OwQxPBP4yFK73E45TI",
+    rarity: "Common",
+    image:
+      "https://arweave.net/u9jMrb_iyiN41w3Sb6m2Y_4KPKqyPQJdLfWt-fAghQQ?ext=png",
+    magicEdenLink: "https://www.tensor.trade/trade/backpack_drip",
+  },
+
 ];
