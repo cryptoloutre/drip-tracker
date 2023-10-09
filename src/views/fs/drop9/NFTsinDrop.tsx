4c8f5e2ee9532ffbb034cb2aec525d61e223d355
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-10-09 09:51:57 +0200

    add drop9 finding satoshi

---
+++ src/views/fs/drop9/NFTsinDrop.tsx
@@ -0,0 +1,9 @@
+export const NFTsinDrop = [
+  {
+    name: 'DRiP Heroes',
+    uri: 'https://arweave.net/bHEWmgj--iE8gqg1sgiSiQKDnzG_wrwxooltRicZa3E',
+    rarity: "Common",
+    image: 'https://arweave.net/5J64284GKuVuKtbuwnD17tX5qbXBtqokx4d17xxr6-A?ext=png',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  }
+];
--- src/views/fs/home/index.tsx
+++ src/views/fs/home/index.tsx
@@ -382,6 +382,19 @@ export const FSHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 8</div>
               </Link>
+              <Link
+                href="/fs/drop9"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/5J64284GKuVuKtbuwnD17tX5qbXBtqokx4d17xxr6-A?ext=png"
+                    alt="drop 9 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 9</div>
+              </Link>
             </div>
           </div>
         </div>
