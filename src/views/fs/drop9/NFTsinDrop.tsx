f9e655d9ba4b129a26c948ebebf97671045f9b4a
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-10-09 09:52:51 +0200

    add drop9 finding satoshi

--- src/views/fs/drop9/NFTsinDrop.tsx
+++ src/views/fs/drop9/NFTsinDrop.tsx
@@ -1,6 +1,6 @@
 export const NFTsinDrop = [
   {
-    name: 'DRiP Heroes',
+    name: 'DRiP FSL Unity',
     uri: 'https://arweave.net/bHEWmgj--iE8gqg1sgiSiQKDnzG_wrwxooltRicZa3E',
     rarity: "Common",
     image: 'https://arweave.net/5J64284GKuVuKtbuwnD17tX5qbXBtqokx4d17xxr6-A?ext=png',
