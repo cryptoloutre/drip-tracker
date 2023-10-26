eb368f5afd79983629f1e41b56b96907e8fb6c1b
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-10-26 16:04:09 +0200

    add drop4 dreader

--- src/views/dreader/drop4/NFTsinDrop.tsx
+++ src/views/dreader/drop4/NFTsinDrop.tsx
@@ -1,26 +1,26 @@
 export const NFTsinDrop = [
   {
-    name: "Adventures through the metaverse",
-    uri: "https://arweave.net/FshZWqMeaMFoxC1FenDy6i8EDWptLZLQB14rUNwcqWg",
-    rarity: "Common",
-    image:
-      "https://arweave.net/t5AkZsrfOb__kUHya_a0dIMrRhzOxo8Wpdg_He8db7g?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/dreader_drip_season_1",
+    name: 'Adventures through the metaverse',
+    uri: 'https://arweave.net/7c2DsRz6uicWG_f8PxkfrflrPN83HCBNUR2JTKhzLcI',
+    rarity: 'Common',
+    image: 'https://arweave.net/i2M7jxtsGf7Fh5xyV2LKZqHiuNWutx218QAD7KZwv1A?ext=png',
+    description: 'Comic collectibles from dReader',
+    magicEdenLink: 'https://www.tensor.trade/trade/dreader_drip_season_1'
   },
   {
-    name: "Adventures through the metaverse",
-    uri: "https://arweave.net/49yxFCxbHxvrQRnPV63COrcNDy2ZWnjWpcbzQlRHbHo",
-    rarity: "Rare",
-    image:
-      "https://arweave.net/FSKk7O9CMc8NcUMJcQNvOXk8Ao69IYMkzO7F4z-aHeM?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/dreader_drip_season_1",
+    name: 'Adventures through the metaverse',
+    uri: 'https://arweave.net/54pIIvlBRTcpkygXMFZ3hOFuMtLG6il5pF_nRBl5OFw',
+    rarity: 'Rare',
+    image: 'https://arweave.net/5Y8sW_fCcxHDMsd8VbOnRMeo1x_kdGejWZNbc0XS7lU?ext=png',
+    description: 'Comic collectibles from dReader',
+    magicEdenLink: 'https://www.tensor.trade/trade/dreader_drip_season_1'
   },
   {
-    name: "Adventures through the metaverse",
-    uri: "https://arweave.net/SYyosATbeYlPXuOj53gCPs9D890Z5AtHq2uU0b0jWak",
-    rarity: "Legendary",
-    image:
-      "https://arweave.net/xPeCT7ilhqtnoELrQ9bxE8gJ8jlBuGWb-J3SDs-AE6s?ext=png",
-    magicEdenLink: "https://www.tensor.trade/trade/dreader_drip_season_1",
-  },
+    name: 'Adventures through the metaverse',
+    uri: 'https://arweave.net/j8dj3TOImNahfqfxfuURD-JvgumcfCTxZjDhXfk2DoU',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/D8oQOBTJq7ueK5VcsWfxdgSVtOUpVQQrrwRomXOLPsY?ext=png',
+    description: 'Comic collectibles from dReader',
+    magicEdenLink: 'https://www.tensor.trade/trade/dreader_drip_season_1'
+  }
 ];
