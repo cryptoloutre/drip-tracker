a632d0e86292128da55dca1ae60827f478124031
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-10-28 11:10:54 +0200

    add drop4 vfx

---
+++ src/views/vfxfreek/drop4/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'Conception',
+    uri: 'https://arweave.net/XXCklfGO4j6EUyfbMLO0iDbJoqx4qWEyZGhFSdLSW1A',
+    rarity: 'Common',
+    image: 'https://arweave.net/vEUebncWVANEPdKeMOjslJ-R386D53rh5RDViN0LzzU?ext=gif',
+    description: '(i) DRiP name comes from the founders playing around with the word "drop".',
+    magicEdenLink: 'https://www.tensor.trade/trade/vfxfreek_drip_s1'
+  },
+  {
+    name: 'Foundation',
+    uri: 'https://arweave.net/FEBbbxUUbxLU-gZ6GLjnAAPLS5LMIYZbSmxQA-Y9QAQ',
+    rarity: 'Rare',
+    image: 'https://arweave.net/p5ZUfnJ2F_qBswQnTVsAruNiZL48-PVJxVcYh_6BIVY?ext=gif',
+    description: '(i) OCT22 - DRiP was founded.',
+    magicEdenLink: 'https://www.tensor.trade/trade/vfxfreek_drip_s1'
+  },
+  {
+    name: 'Zone DRiP',
+    uri: 'https://arweave.net/1OC8gu8A1oVuJxso5J8MEUGLLE6Dz9khp9ALR0x1vPs',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/Z5F6mft0y7ny7ZtKc82OCHul3lF8G71WTTnFgyq8h4g?ext=gif',
+    description: "(i) DRiP founders' vision is about creating a free and rewarding collecting game.",
+    magicEdenLink: 'https://www.tensor.trade/trade/vfxfreek_drip_s1'
+  }
+];
