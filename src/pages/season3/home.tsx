907bc482890eb28a71e15893dda0cbd4a38be8a8
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-02 15:18:32 +0100

    add drop1 season3

--- src/pages/season3/home.tsx
+++ src/pages/season3/home.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Season2Home } from "../../views";
+import { Season3Home } from "../../views";
 
 const Home: NextPage = (props) => {
   return (
@@ -12,7 +12,7 @@ const Home: NextPage = (props) => {
           content="DRiP tracker allows you to visualize which DRiP NFTs you miss. It also provides information about the artist and the drop in particular."
         />
       </Head>
-      <Season2Home />
+      <Season3Home />
     </div>
   );
 };
