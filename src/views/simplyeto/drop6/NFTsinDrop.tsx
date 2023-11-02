55112408cc7ddd9f8568f66292f5c97e18ab61b1
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-02 09:04:54 +0100

    add drop6 simplyeto

---
+++ src/views/simplyeto/drop6/NFTsinDrop.tsx
@@ -0,0 +1,34 @@
+export const NFTsinDrop = [
+  {
+    name: 'From Surakarta, Central Java',
+    uri: 'https://arweave.net/vn6mEKTdwgO_5yY2WOm2JjOUT5R68BfrhI4jdA4yHHE',
+    rarity: 'Common',
+    image: 'https://arweave.net/mxoLRWK5iWvIOBmm38dVIVnKEwTRTXubW6Iv7imp24c?ext=png',
+    description: 'The watercolor, gouache and ink version of a building in Surakarta, Java',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'From Surakarta, Central Java',
+    uri: 'https://arweave.net/j8MpUb_2ZhwO-uUBJXrdC4--MNSZWkr_zP1lY5PlRes',
+    rarity: 'Rare',
+    image: 'https://arweave.net/sw4D35uKfzBukUIEufJKb10O_iv1rw-deEE44356xnQ?ext=png',
+    description: 'Indulge in the sounds, lights, and passage of time of the crowded city of Surakarta, Java',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'From Surakarta, Central Java',
+    uri: 'https://arweave.net/3nhYvm-T0R88csnT7VKZT7HPXj2Bp-O2Z7AvMLemZbQ',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/oDYOMoIGmKfD9JH0ut8Ua7H32l3HL0HxiXfRity09mE?ext=png',
+    description: 'Be blown away by the movement and feelings of this triangular shaped building at Surakarta, Java',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'From Surakarta, Central Java',
+    uri: 'https://arweave.net/WSn4ilbC_do030EC3AKIDYSyfZt9E4WzVC6A8esw4mo',
+    rarity: 'Ultimate',
+    image: 'https://arweave.net/ljKVU06BpPFRxfqXooF9XxVe8-MjTzO-ZMY9dpbGslc?ext=png',
+    description: 'The process is even more valuable than the piece itself. This is how the handmade piece was done.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  }
+];
--- src/views/simplyeto/home/index.tsx
+++ src/views/simplyeto/home/index.tsx
@@ -337,6 +337,19 @@ export const SimplyEtoHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 5</div>
               </Link>
+              <Link
+                href="/simplyeto/drop6"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/ljKVU06BpPFRxfqXooF9XxVe8-MjTzO-ZMY9dpbGslc?ext=png"
+                    alt="drop 6 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 6</div>
+              </Link>
             </div>
           </div>
         </div>
