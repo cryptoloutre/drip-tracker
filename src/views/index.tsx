fbe9e88ffe9882ef7cc076bc2cddceadbda48340
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-12 10:06:50 +0100

    add drop19 maquin

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -244,6 +244,7 @@ export {Drop15Maquin} from "./maquin/drop15";
 export {Drop16Maquin} from "./maquin/drop16";
 export {Drop17Maquin} from "./maquin/drop17";
 export {Drop18Maquin} from "./maquin/drop18";
+export {Drop19Maquin} from "./maquin/drop19";
 export {GenefteeHome} from "./geneftee/home";
 export {Drop1Geneftee} from "./geneftee/drop1";
 export {Drop2Geneftee} from "./geneftee/drop2";
---
+++ src/views/maquin/drop19/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'Torch',
+    uri: 'https://arweave.net/IzT8IE5hmXYokgJBIKQzceBPZQvtdIyCFCzTnUpsA2Y',
+    rarity: 'Common',
+    image: 'https://arweave.net/4b9jrMtW-TtS0hYwTDNX2dKFaNG-M3uIefU4OqIJGjs?ext=jpg',
+    description: 'Microscopic composition resembling a fire.',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Inferno',
+    uri: 'https://arweave.net/0QyBNaIn5jZ5f7wJwzrC05sNW-EIq-fE8ldka3Dhz1w',
+    rarity: 'Rare',
+    image: 'https://arweave.net/NCLg7VJjjbnLsa2xJVmUESzf7VMpmDGY-UKuXYZIo2c?ext=jpg',
+    description: 'Color tuning to give intensity to flames.',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  },
+  {
+    name: 'Arctic Burn',
+    uri: 'https://arweave.net/K1y4TEJscFhn-C5Mhwfr2umTIWhTFKtHsGmsld_armk',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/OsYRvAycVebpPK6pX61xRuzsVh_nXDBxqOHtyuqyQWI?ext=jpg',
+    description: 'Invert colors tuning to give this cold burning feeling.',
+    magicEdenLink: 'https://www.tensor.trade/trade/maquin_drip_season_1'
+  }
+];
