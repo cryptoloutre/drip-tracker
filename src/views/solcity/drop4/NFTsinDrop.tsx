89f6822a75e7a9941642478160e37f5821c6dd0a
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-13 09:28:24 +0100

    add drop4 solcity

---
+++ src/views/solcity/drop4/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: '3TV & Collector in Breakpoint',
+    uri: 'https://arweave.net/TmR_BvtUFRXa319GZOsKbNFE-s9cKWmlxLnF4Ow0th0',
+    rarity: 'Common',
+    image: 'https://arweave.net/RvwdqI9OQapWwnKbNUBKuW6Nek7rbANwOOQbhoy7xaA?ext=gif',
+    description: 'Kei Gowda talks to 3Land founder Bangerz about her Breakpoint exhibition powered by Collector. Nate, the man behind Collector.sh, is also there… and we are doxxing him, hard.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Breakpoint Bloopers',
+    uri: 'https://arweave.net/UKSQEVdr5W19rWjIb39twBgB8hKpl5SvUxXMDBDzBm4',
+    rarity: 'Rare',
+    image: 'https://arweave.net/nPtPAYk9hE3iHHQg84ue6YospuYoDtGyLqpXB1JbFso?ext=png',
+    description: 'You are one out of 250 people who gets to see these bloopers, HODL.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Two Girlz One Umbrella',
+    uri: 'https://arweave.net/FgdJXKw3y1eGNMJnPRljV0B9hqMH52ZW-SXxRXzkR7I',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/u3neC7izJeJNbVbUv5BiaU8849rnBnLJwX4_D0KawXw?ext=png',
+    description: 'Bangerz & Neverland making out under a mushroom umbrella while Kei, the caterpillar, and JOM, the acorn.. lurker in the background.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  }
+];
