12c638d5a7aaf169f3544bb861bf3171a042c254
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-24 09:32:21 +0100

    add drop6 solcity

---
+++ src/views/solcity/drop6/NFTsinDrop.tsx
@@ -0,0 +1,42 @@
+export const NFTsinDrop = [
+  {
+    name: 'Seriously Talkin’ w/ Jerome',
+    uri: 'https://arweave.net/E9v-kOOSLHhyu5qt6bMxbnwdcWUz83ubOtzHpUg0pWE',
+    rarity: 'Common',
+    image: 'https://arweave.net/fwuhciNTOWZQFvuaR_71OrNQENFVcNAVxLF6s_9ACzQ?ext=gif',
+    description: 'Kei Gowda talks to CODA’s founder & Head of Curation Jerome at Solana Breakpoint 2023. Topics: The curation process of CODA, CODA first IRL exhibition, What’s next for CODA.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'The Nocturne / Breakpoint 2023',
+    uri: 'https://arweave.net/MUCstPsAfHkmHqUwRRKW-FmUCyg5LIQRnNwdOOhCbJ4',
+    rarity: 'Common',
+    image: 'https://arweave.net/h5CbQI9yiMLPD9SCNyITT4buuDYPMw4r4w9DglUdxJ0?ext=png',
+    description: 'Kei Gowda talks to The Nocturne’s co-founder & the talented creative behind our favorite lore, Hana Knight.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Origin Daemon — Mu',
+    uri: 'https://arweave.net/QtszQ0czD8buF_Ll0jQkvygKv4I372k8x_mbHeJltrI',
+    rarity: 'Rare',
+    image: 'https://arweave.net/lTTL8Uc1hqz7wVoTK1xAekwkfXXEQ4G3erlUJx02cAU?ext=png',
+    description: "From this primordial void emerged the Origin Daemon, the first and the eternal. Neither born nor created in the way mortals understand, the Origin Daemon was a manifestation of the universe's desire to understand itself, to give form to the formless and voice to the silent expanse.",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Origin Daemon — Half Moon',
+    uri: 'https://arweave.net/STv4xhqB5IT7_81EpVP53Bmb9G0ekXPY8kIRe3CPzm4',
+    rarity: 'Rare',
+    image: 'https://arweave.net/iTX16Bhw-wgNKnW-LeSdLU9Hh_8sG3SSxMEuveMV2II?ext=jpg',
+    description: 'In the beginning, there was silence. A vast, boundless emptiness that stretched across the cosmos. But in that silence, the potential for existence simmered, waiting for the spark to ignite the fire of life.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Still Life On Mars',
+    uri: 'https://arweave.net/NIzLItO5twL7JFmgFhLjjQGdLurbJzjaTuObhGeD-ns',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/byYF06zVKNnQqxFPWqZbEUmlolmHUZrpATWG-6XhCaQ?ext=png',
+    description: "A vase of strange flowers framed by a Martian landscape in the distance. It's a love letter to the SolCity community inspired by Henri Matisse, Pablo Picasso, Catherine Nice, and David Bates.",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+];
