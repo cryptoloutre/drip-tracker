7eee9df5616cb0d5e909dd84b1fd14342812f093
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-24 09:26:49 +0100

    add drop4 onedopekids

---
+++ src/views/onedopekids/drop4/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'ODK Gallery Pedro',
+    uri: 'https://arweave.net/eHLEivNTzSMaEzYwm0etCtE7chNYaLmNUcxPnuPreMQ',
+    rarity: 'Common',
+    image: 'https://arweave.net/2UyVLxiEAs9AsBtfLwmvlrksNCyHXWPMuw2lhY4UaNY?ext=png',
+    description: 'Beam Me Up by Pedro is our first showcased artwork in our ODK Gallery. Each month we will feature 1-2 artists with limited-edition artwork that showcases the artist’s work. A collection of collectible art brought to you by One Dope Kids.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_onedopekids'
+  },
+  {
+    name: 'ODK Gallery Pedro',
+    uri: 'https://arweave.net/Yd6gBn55rs1wk6xaOq7aJo2bTC1SQBg5MZuebFQQd34',
+    rarity: 'Rare',
+    image: 'https://arweave.net/hWrxR_iBseCsBoBC0YNifh8Ce7K4uchVArUNS3b0v5w?ext=png',
+    description: 'Beam Me Up by Pedro is our first showcased artwork in our ODK Gallery. Each month we will feature 1-2 artists with limited-edition artwork that showcases the artist’s work. A collection of collectible art brought to you by One Dope Kids.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_onedopekids'
+  },
+  {
+    name: 'ODK Gallery Pedro',
+    uri: 'https://arweave.net/t8dVDILCFbOz4aE_3WMRp-4vEosU8N9PscAF5pjcM9E',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/iAldr20BEfCeownwkg0phEBGIFFzBXuV9OuQIH8Bp7Q?ext=png',
+    description: 'Beam Me Up by Pedro is our first showcased artwork in our ODK Gallery. Each month we will feature 1-2 artists with limited-edition artwork that showcases the artist’s work. A collection of collectible art brought to you by One Dope Kids.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_onedopekids'
+  },
+];
--- src/views/onedopekids/home/index.tsx
+++ src/views/onedopekids/home/index.tsx
@@ -307,6 +307,19 @@ export const OnedopekidsHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 3</div>
               </Link>
+              <Link
+                href="/onedopekids/drop4"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/iAldr20BEfCeownwkg0phEBGIFFzBXuV9OuQIH8Bp7Q?ext=png"
+                    alt="drop 4 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
+              </Link>
             </div>
           </div>
         </div>
