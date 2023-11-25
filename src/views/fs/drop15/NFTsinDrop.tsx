943229971a642ae604580b68da8f0e37f1e1505c
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-11-25 10:19:08 +0100

    add drop15 fs

---
+++ src/views/fs/drop15/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'DRiP Thankful Turkeys',
+    uri: 'https://arweave.net/5jBAtVtK06QsmjbVLjsXX17N-BrxOBzwFTzSKmZDPD0',
+    rarity: 'Common',
+    image: 'https://arweave.net/kUB7zE0XIWqbiTUmDRezWECEqxHsy7759oZMSP5BHdw?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT prompts by Madivaan: Adorable cartoonish Thanksgiving turkey sitting on a wooden table, wearing a whimsical pilgrim hat, pumpkin pie dish, scattered autumn leaves, Thanksgiving festive theme, candles, side view, window, autumn scenery, orangecore, cartoon, digital art GNT prompts by Tatsiana: A thanksgiving turkey, paper quilling, paper cut art, paper illustration, extreme detail, 8k GNT prompts by CioSilv: funny turkey,  caricature, splash color, hd, thanksgiving background',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  },
+  {
+    name: 'DRiP Thankful Turkeys',
+    uri: 'https://arweave.net/PsCSPKxL_No9LsaoZQRS7fXoExJ8Hpeh42S6s30jxjU',
+    rarity: 'Rare',
+    image: 'https://arweave.net/3cuOsnLcYsZfngzobTbKn-1RYFrmtUj15y6eoJddAR0?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT prompts by Madivaan: Adorable cartoonish Thanksgiving turkey sitting on a wooden table, wearing a whimsical pilgrim hat, pumpkin pie dish, scattered autumn leaves, Thanksgiving festive theme, candles, side view, window, autumn scenery, orangecore, cartoon, digital art GNT prompts by Tatsiana: A thanksgiving turkey, paper quilling, paper cut art, paper illustration, extreme detail, 8k GNT prompts by CioSilv: funny turkey,  caricature, splash color, hd, thanksgiving background',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  },
+  {
+    name: 'DRiP Thankful Turkeys',
+    uri: 'https://arweave.net/x7LUmJ2GUQJ8YuaFxZawCeOacDNpuntsNWOknnvVMPk',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/D09xwvdOQqMmtE9qex_IHYKVaQodL80GHn7cvQC9Mms?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT prompts by Madivaan: Adorable cartoonish Thanksgiving turkey sitting on a wooden table, wearing a whimsical pilgrim hat, pumpkin pie dish, scattered autumn leaves, Thanksgiving festive theme, candles, side view, window, autumn scenery, orangecore, cartoon, digital art GNT prompts by Tatsiana: A thanksgiving turkey, paper quilling, paper cut art, paper illustration, extreme detail, 8k GNT prompts by CioSilv: funny turkey,  caricature, splash color, hd, thanksgiving background',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  }
+];
--- src/views/fs/home/index.tsx
+++ src/views/fs/home/index.tsx
@@ -460,6 +460,19 @@ export const FSHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 14</div>
               </Link>
+              <Link
+                href="/fs/drop15"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/D09xwvdOQqMmtE9qex_IHYKVaQodL80GHn7cvQC9Mms?ext=png"
+                    alt="drop 15 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 15</div>
+              </Link>
             </div>
           </div>
         </div>
