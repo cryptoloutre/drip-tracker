46cf133b2b119cb037a350ebe80408c05ff4c0d5
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-12-04 09:21:08 +0100

    add drop6 enigma

---
+++ src/views/enigma/drop6/NFTsinDrop.tsx
@@ -0,0 +1,50 @@
+export const NFTsinDrop = [
+  {
+    name: "doodle('boot');",
+    uri: 'https://arweave.net/uHk51ff15iPfsv_YqDW3Ehre4Etu2X8mzd-SEK2gL-I',
+    rarity: 'Common',
+    image: 'https://arweave.net/j-c3KXZPp2-FzGriDDayTH5gfDiYRc5QUUfN8THZRjM?ext=png',
+    description: "doodle_cd(#00ff00); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  },
+  {
+    name: "doodle('deflect');",
+    uri: 'https://arweave.net/C8NDY1_cI8cLq3E_GR6oPtO0bAuoS0ShEmQxL8Pw2OQ',
+    rarity: 'Common',
+    image: 'https://arweave.net/gJf4yjXXdDA-EBBaYdHvXae6EXB7o3F5mN9Lt2l1R-w?ext=png',
+    description: "doodle_cd(#00ffff); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  },
+  {
+    name: "doodle('dodge');",
+    uri: 'https://arweave.net/WQqwl7awrwKiLTF__ni--MspsLc0MT6leT8G-1xDsIc',
+    rarity: 'Rare',
+    image: 'https://arweave.net/S6uy2LA5HbxonUo-4hDysIJHikkZVjlwZnUA_KbVxWs?ext=png',
+    description: "doodle_cd(#ff0000); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  },
+  {
+    name: "doodle('pique');",
+    uri: 'https://arweave.net/QN4aEzBziiySksFa0YK5l_4teeu2ekMZuxXIkORKcjg',
+    rarity: 'Rare',
+    image: 'https://arweave.net/TRFgZcwQdBqCSPs9q29Gw97qSmXhWwooJvi6_n6VMYI?ext=png',
+    description: "doodle_cd(#ff00ff); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  },
+  {
+    name: "doodle('focus');",
+    uri: 'https://arweave.net/MX-JzTWWwk5pkXdgFEK1TlLIiRX7uaqrKHZdr6brRHY',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/QEM3Xb0uXB8c-aKa08JhfpUsE-9MQsfgCZER4lDdo7k?ext=png',
+    description: "doodle_cd(#ff840a); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  },
+  {
+    name: "doodle('stride');",
+    uri: 'https://arweave.net/eDht0ICuoP2kbsghO9yWDAF-kma7NzKCM8NR1ECIpX8',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/gj-8MZ3OaFhwsc0NoeXby5ueE0f78LogODHRommjYc4?ext=png',
+    description: "doodle_cd(#000000); doodle_searchpoints(5); ENiGMA's .glb doodles expressing human reactions.",
+    magicEdenLink: 'https://www.tensor.trade/trade/enigma_drip'
+  }
+];
--- src/views/enigma/home/index.tsx
+++ src/views/enigma/home/index.tsx
@@ -335,6 +335,19 @@ export const EnigmaHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 5</div>
               </Link>
+              <Link
+                href="/enigma/drop6"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/gj-8MZ3OaFhwsc0NoeXby5ueE0f78LogODHRommjYc4?ext=png"
+                    alt="drop 6 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 6</div>
+              </Link>
             </div>
           </div>
         </div>
