dd318d646851b4dd88d565c6ab9333b83f0f94cc
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-12-07 10:40:22 +0100

    add drop11 simplyeto

---
+++ src/views/simplyeto/drop11/NFTsinDrop.tsx
@@ -0,0 +1,34 @@
+export const NFTsinDrop = [
+  {
+    name: 'Framed',
+    uri: 'https://arweave.net/NBE9__wCifrCOYELJYW7WIuYlADQq-6pGGPsh2HClDg',
+    rarity: 'Common',
+    image: 'https://arweave.net/uFbRP9JYF3098OtJNpc2sY3zuu4aIuphOTGfcVBM2D8?ext=png',
+    description: 'The cold mornings reflect slow life in the city. A winter is creeping in.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'Framed',
+    uri: 'https://arweave.net/Ef7wokNVMD4w4DCrJZ1XH7FcJwTurjFGa4mxMZ_6Qvo',
+    rarity: 'Rare',
+    image: 'https://arweave.net/Ijy55lP01GaUwc8EXdokp6GW9-Hye13qjfHNgImnhBo?ext=png',
+    description: "Symmetry and dryness. Nature can't keep up with the change in weather. We persist.",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'Framed',
+    uri: 'https://arweave.net/PIxPtDgO-cAT3KV5Sa3E4J6DvahcevwJc7MdVD3HdLw',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/RGZGP1K2EcfMnRS1RlnovARzY0ZGHHB_QCQjOT09U5k?ext=png',
+    description: 'Colors, abstractions, contrast.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  },
+  {
+    name: 'Framed',
+    uri: 'https://arweave.net/PZMn8HsTuoCSkTjwwv27se4wqIoJB6iB-hWnhAq5AhE',
+    rarity: 'Ultimate',
+    image: 'https://arweave.net/eNz5OPomO0q020imkMazTQj2_q-Y_55eTPI8D8Q_c4E?ext=png',
+    description: "We live in a life of contrast. Warm and cold, there's nothing but our clear search of balance.",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_thegems_s1'
+  }
+];
