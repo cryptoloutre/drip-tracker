6412b2150bcb17aa26983e565dee4681e205f763
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-12-11 16:56:41 +0100

    add drop7 solcity

---
+++ src/views/solcity/drop7/NFTsinDrop.tsx
@@ -0,0 +1,50 @@
+export const NFTsinDrop = [
+  {
+    name: 'Pier Story',
+    uri: 'https://arweave.net/lKmv6Ew1b8Iy3ZuswMf-8H4sJqSpJhjsvvk8G8Bei9M',
+    rarity: 'Common',
+    image: 'https://arweave.net/TKZnvoX3xEORXYPVWixASAcgXuME63fMMuHzctPPnEM?ext=png',
+    description: 'Lahseminit x 82dazai',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'The Gentlemen',
+    uri: 'https://arweave.net/kAfz-PsJROtR6u1kXYqQFoLCDSwcZGgM6PSXskWvMko',
+    rarity: 'Rare',
+    image: 'https://arweave.net/IT1pI7EU8PwylEycKrai5atZuYfHBO4hDLAFtL797Ng?ext=png',
+    description: 'Lahseminit x Imaji Daniel',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Love & Butterfly',
+    uri: 'https://arweave.net/uBp4UbdMWvZLDVZD-pLVu6eMIDa5jbP2sHvG7MntAu4',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/4dKJpLXC1xKTwPl38Lmyb6NQHz1_BXyMnPI-LvFZzws?ext=jpg',
+    description: 'Imaji Daniel x 82dazai',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Hanging Rose',
+    uri: 'https://arweave.net/xwOI7A_ZjTzJXhq-uOWzOh8Jd2XZxue6y8o11FJnhiM',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/zCDEiWzYqBhmbYA240lYS9oc4g55Cs31_ZYxUDlxLro?ext=jpg',
+    description: 'Imaji Daniel x 82dazai',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Sponge Hair',
+    uri: 'https://arweave.net/oVfS70ao47jm-53DcM4tqpP6DAbjRGYlbAKzwvzU8C8',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/WU6dKInKbtYsPyBCloMYpVvijtCX4O_yRsvopVbxGLU?ext=jpg',
+    description: 'Imaji Daniel x 82dazai',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Split',
+    uri: 'https://arweave.net/dfFH7MQCOx8Kx5mSop3AA9nIHmzzMpk3qXPpy3_RL8k',
+    rarity: 'Ultimate',
+    image: 'https://arweave.net/bYhrlixQmhoaIDPwCrWQIUjTgS8Kj8It_HWJPsdX8_g?ext=jpg',
+    description: 'Lahseminit x Imaji Daniel x 82dazai',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  }
+];
