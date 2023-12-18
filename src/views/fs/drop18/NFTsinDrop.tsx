9b1cd093549b9d14280dc981f608fd87ec5ef71a
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-12-18 09:54:09 +0100

    add drop18 fs

---
+++ src/views/fs/drop18/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'MOOAR DRiP Droplets',
+    uri: 'https://arweave.net/4ZnZXD-DPdT6VHdoz1Jp0qG3sgTchyGxq8SyFJYqFcU',
+    rarity: 'Common',
+    image: 'https://arweave.net/J5HBlLe_u9eJbiTxbVFDL_w_-QLdtxcb60X-Hr0e_8k?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT Prompt by @_Yuha__:  Cute smiling robot with big cute eyes wearing Santa Claus hat and red sneakers, Christmas environment, SCIFI, published concept art, illustration, realistic, 8K, object detection full HD intricate, highly detailed, digital painting, artstation, smooth, sharp focus, cinematic, VFX. GNT Prompt by @atozaltema: A dynamic, full-bodied female character inspired by the spirit of Christmas, equipped with a festive staff topped with a stylized Christmas tree. They wear a vibrant Santa hat and a fluffy, richly-textured Santa coat with intricate patterns. Their attire includes holiday-themed armor with candy cane stripes and holly berry accents. The boots are reindeer-fur inspired, and their expression is one of festive cheer, embodying the joy and energy of the holiday season. This character stands amidst a snowy backdrop. GNT Prompt by @SurvivorD_STEPN: Godfather, in the Christmas scene.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  },
+  {
+    name: 'MOOAR DRiP Droplets',
+    uri: 'https://arweave.net/09LTF0PDo3UQLsu8yTK1XFtnXUbD_BPvQ9DtmduQFMQ',
+    rarity: 'Rare',
+    image: 'https://arweave.net/8FdAMWR5zL8_Qv8Oq-BKbR0z-doZ7OHfrTEo3xQyQls?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT Prompt by @_Yuha__:  Cute smiling robot with big cute eyes wearing Santa Claus hat and red sneakers, Christmas environment, SCIFI, published concept art, illustration, realistic, 8K, object detection full HD intricate, highly detailed, digital painting, artstation, smooth, sharp focus, cinematic, VFX. GNT Prompt by @atozaltema: A dynamic, full-bodied female character inspired by the spirit of Christmas, equipped with a festive staff topped with a stylized Christmas tree. They wear a vibrant Santa hat and a fluffy, richly-textured Santa coat with intricate patterns. Their attire includes holiday-themed armor with candy cane stripes and holly berry accents. The boots are reindeer-fur inspired, and their expression is one of festive cheer, embodying the joy and energy of the holiday season. This character stands amidst a snowy backdrop. GNT Prompt by @SurvivorD_STEPN: Godfather, in the Christmas scene.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  },
+  {
+    name: 'MOOAR DRiP Droplets',
+    uri: 'https://arweave.net/miJ1tAx1VcT7EfOdR_hlCOUtmEbuful5JsInydjW0EA',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/rDbCnDr3lidyn6HiXtUwglsDMUwScSCOgnRaZNiKM0A?ext=png',
+    description: 'AI Generated Art using GNT on MOOAR.com (http://mooar.com/) from Find Satoshi Lab, the creators of STEPN, MOOAR and Gas Hero. GNT Prompt by @_Yuha__:  Cute smiling robot with big cute eyes wearing Santa Claus hat and red sneakers, Christmas environment, SCIFI, published concept art, illustration, realistic, 8K, object detection full HD intricate, highly detailed, digital painting, artstation, smooth, sharp focus, cinematic, VFX. GNT Prompt by @atozaltema: A dynamic, full-bodied female character inspired by the spirit of Christmas, equipped with a festive staff topped with a stylized Christmas tree. They wear a vibrant Santa hat and a fluffy, richly-textured Santa coat with intricate patterns. Their attire includes holiday-themed armor with candy cane stripes and holly berry accents. The boots are reindeer-fur inspired, and their expression is one of festive cheer, embodying the joy and energy of the holiday season. This character stands amidst a snowy backdrop. GNT Prompt by @SurvivorD_STEPN: Godfather, in the Christmas scene.',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_finding_satoshi_season_1'
+  }
+];
--- src/views/fs/home/index.tsx
+++ src/views/fs/home/index.tsx
@@ -499,6 +499,19 @@ export const FSHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 17</div>
               </Link>
+              <Link
+                href="/fs/drop18"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/rDbCnDr3lidyn6HiXtUwglsDMUwScSCOgnRaZNiKM0A?ext=png"
+                    alt="drop 18 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 18</div>
+              </Link>
             </div>
           </div>
         </div>
