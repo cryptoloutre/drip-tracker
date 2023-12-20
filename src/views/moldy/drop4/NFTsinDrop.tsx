b45d2303bee148b4b953f749166d15b652d8de4a
cryptoloutre <miningcryptomonnaie@gmail.com>
2023-12-20 10:01:05 +0100

    add drop4 moldy

---
+++ src/views/moldy/drop4/NFTsinDrop.tsx
@@ -0,0 +1,29 @@
+export const NFTsinDrop = [
+  {
+    name: 'Afloat',
+    uri: 'https://arweave.net/yr3AI9_bx-mpuMT6ZJ88fwTbpZnUKOjIX0Xi1pAB_4o',
+    rarity: 'Common',
+    image: 'https://arweave.net/CUGxJNcKHvW807gl0mUVuX6ObZ7aPsK1vf-DgMmkaaQ?ext=png',
+    description: '“Afloat” is a 1/1 textile piece created out of a variety of upcycled garments cut and sewn on to a 6 x 6 inch raw canvas base. \n' +
+      "The physical art work is available via Mallow as this drop's Ultimate release!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_moldy'
+  },
+  {
+    name: 'Afloat',
+    uri: 'https://arweave.net/RcKASh5s61cYN3GhMlZSokT26SOHGrXVn3SKM5KN0Ss',
+    rarity: 'Rare',
+    image: 'https://arweave.net/1iUIbZqLZjlXBcr7FX1IIqR1KrU2KjPCxKTGagcn3pk?ext=png',
+    description: '“Afloat” is a 1/1 textile piece created out of a variety of upcycled garments cut and sewn on to a 6 x 6 inch raw canvas base. \n' +
+      "The physical art work is available via Mallow as this drop's Ultimate release!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_moldy'
+  },
+  {
+    name: 'Afloat',
+    uri: 'https://arweave.net/dg2QjhmdgLr8KjwBk1_2GCXIu75w4mF_V7sG6uy8QYw',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/APaTbgEXayV16u2kr1g13M07UFXaegjypS1j5Urgxqg?ext=png',
+    description: '“Afloat” is a 1/1 textile piece created out of a variety of upcycled garments cut and sewn on to a 6 x 6 inch raw canvas base. \n' +
+      "The physical art work is available via Mallow as this drop's Ultimate release!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_moldy'
+  }
+];
