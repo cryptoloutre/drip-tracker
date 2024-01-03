6b4cf8904176acc7f05f7522ee1df3bf6b6913dc
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-03 10:47:47 +0100

    fix 0xgraffito

--- src/views/0xgraffito/drop1/index.tsx
+++ src/views/0xgraffito/drop1/index.tsx
@@ -20,7 +20,7 @@ export const Drop1Graffito: FC = ({}) => {
   const [nbUserNFTs, setNbUserNFTs] = useState<number>();
 
   const dropNb = "Proof by Infinite Descent";
-  const NFTsinDrop = DropInfo[Number(dropNb)-1].NFTs;
+  const NFTsinDrop = DropInfo.find((drop) => drop.dropNb == dropNb).NFTs;
   const nbTotalNFTsInDrop = NFTsinDrop.length;
   const NFTsInThisDrop = NFTsinDrop;
 
