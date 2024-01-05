749a850d80959f5b80bf3325da8022951e2744cc
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-05 10:49:21 +0100

    add components

--- src/views/fs/home/index.tsx
+++ src/views/fs/home/index.tsx
@@ -11,6 +11,7 @@ import { Loader } from "components/Loader";
 import { WrapperConnection } from "../../../../ReadApi/WrapperConnection";
 import { DropInfo } from "./DropInfo";
 import { getUserNFTs } from "utils/getUserNFTs";
+import { Completion } from "components/Completion";
 
 export const FSHome: FC = ({}) => {
   const wallet = useWallet();
@@ -217,63 +218,7 @@ export const FSHome: FC = ({}) => {
             </div>
           )}
           {(wallet.publicKey || isXNFT) && isFetched && (
-            <div className="text-center w-[70%] mx-auto font-bold text-xl my-6">
-              <div className="text-left">
-                You have:
-                <div className="">
-                  <div>
-                    • completed{" "}
-                    <span className="font-black text-[#14F195]">
-                      {nbDropComplete}
-                    </span>
-                    /
-                    <span className="font-black text-[#14F195]">
-                      {nbTotalDrop}
-                    </span>{" "}
-                    drops!
-                  </div>
-                  <div>
-                    •{" "}
-                    <span className="font-black text-[#14F195]">
-                      {nbUserNFTs}
-                    </span>
-                    /
-                    <span className="font-black text-[#14F195]">
-                      {nbTotalNFTsInDrop}
-                    </span>{" "}
-                    NFTs!
-                  </div>
-                  {dropIncomplete.length != 0 && (
-                    <div>
-                      Drop incomplete:
-                      <div className="flex ml-4">
-                        →
-                        {dropIncomplete.map((drop) => (
-                          <div key={drop} className="mx-1 text-[#ff0000]">
-                            {drop}
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                  {dropMissing.length != 0 && (
-                    <div>
-                      Drop missed:
-                      <div className="flex ml-4">
-                        →
-                        {dropMissing.map((drop) => (
-                          <div key={drop} className="mx-1 text-[#ff0000] ">
-                            {drop}
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-              <br />
-              <div>Choose a drop to see which NFTs you miss.</div>
-            </div>
+            <Completion nbDropComplete={nbDropComplete} nbTotalDrop={nbTotalDrop} nbTotalNFTsInDrop={nbTotalNFTsInDrop} nbUserNFTs={nbUserNFTs} dropIncomplete={dropIncomplete} dropMissing={dropMissing} />
           )}
           {(wallet.publicKey || isXNFT) && !isFetched && <Loader />}
           <div className="flex justify-center">
