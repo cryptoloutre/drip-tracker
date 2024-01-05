e7a233a54311a3e08c02fdf79cc6381c01dbce10
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-05 18:54:52 +0100

    add drop18 dropation

---
+++ src/views/dropnation/drop18/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'ALARM',
+    uri: 'https://arweave.net/HW_7-PY--Ia_m7g0Gd9odTrW7xua3Qtocw75dcbUNRY',
+    rarity: 'Common',
+    image: 'https://arweave.net/bF3XRr435ur2828cyXYRrGBai4boUPsYKXgYkjMJKW0?ext=jpg',
+    description: "A melodic, dark, and inspiring vocal-centric techno house anthem by Vitamin THC!🔥 The perfect tune to chew glass to! ALARM is the first song in Vitamin THC's fifth DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  },
+  {
+    name: 'ALARM (Vitamin THC Remix)',
+    uri: 'https://arweave.net/TsdtV5IcIPKX1hDDmkQPV6HY8rJewZux6583GogJiiw',
+    rarity: 'Rare',
+    image: 'https://arweave.net/YPWrVCwTo34r6pInvmHrFz1iJ9vfZ99qDLUbCzQZS5U?ext=jpg',
+    description: "A high energy melodic bass house banger by Vitamin THC!🔥 A VIP remix of the original! ALARM (Vitamin THC Remix) is the second song in Vitamin THC's fifth DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  },
+  {
+    name: 'TEAR DROPS (The Last Song)',
+    uri: 'https://arweave.net/yN3Z0baokmsVIa3TW_qjYTv_WZu8ji57YEarFKYD8F0',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/S2TMdEti-yGra0EBKqcRnwXSngvKTA9GYm7UWFV6OQk?ext=jpg',
+    description: "A euphoric, vibey, and bass-heavy melodic techno song by Vitamin THC!✨🌠❤️ The light that makes the darkness disappear... TEAR DROPS (The Last Song) is the third song in Vitamin THC's fifth DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  }
+];
--- src/views/dropnation/home/index.tsx
+++ src/views/dropnation/home/index.tsx
@@ -455,6 +455,19 @@ export const DropNationHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 17</div>
               </Link>
+              <Link
+                href="/dropnation/drop18"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/S2TMdEti-yGra0EBKqcRnwXSngvKTA9GYm7UWFV6OQk?ext=jpg"
+                    alt="drop 18 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 18</div>
+              </Link>
             </div>
           </div>
         </div>
