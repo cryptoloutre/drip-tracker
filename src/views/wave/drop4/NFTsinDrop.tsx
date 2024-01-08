72f92375abc47a77a78a1e72fadf6abdeb09a61d
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-08 08:47:55 +0100

    add drop4 wave

---
+++ src/views/wave/drop4/NFTsinDrop.tsx
@@ -0,0 +1,32 @@
+export const NFTsinDrop = [
+  {
+    name: 'Cube Interview Common',
+    uri: 'https://arweave.net/T6t9_l9REG20Kt5z_krFv9_pzp5hI7Z9L_lmO8ygmgs',
+    rarity: 'Common',
+    image: 'https://arweave.net/ULP3zEczIkFuyCTuzXtwH04cOEHmEM3RtP-GAYfJw-A?ext=png',
+    description: 'In this episode we interviewed the bright minds at Cube Exchange. We talked about how the exchange works, why they made it, how it beats the competition and so much more!\n' +
+      '\n' +
+      'Watch the full interview here: https://www.youtube.com/watch?v=GcDhbJLHG-Q&t=58s',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_wave'
+  },
+  {
+    name: 'Cube Interview Rare',
+    uri: 'https://arweave.net/63yKA-aIscfDeLvsU87-EBXzDdBAC60q4VTzxnAtLPw',
+    rarity: 'Rare',
+    image: 'https://arweave.net/UC2b_djDsSux_2I5Df2CKIzT3CzwS14gukfCxNQdogY?ext=jpg',
+    description: 'In this episode we interviewed the bright minds at Cube Exchange. We talked about how the exchange works, why they made it, how it beats the competition and so much more!\n' +
+      '\n' +
+      'Watch the full interview here: https://www.youtube.com/watch?v=GcDhbJLHG-Q&t=58s',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_wave'
+  },
+  {
+    name: 'Cube Interview Legendary',
+    uri: 'https://arweave.net/GWY09PJJz5sp714AWqXjsrOtq0yTgj8H0ivytlDc5_Q',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/-LrzYsu1Ma0-moekz7sFgSPJyBExLNYztdAcWmDQTck?ext=png',
+    description: 'In this episode we interviewed the bright minds at Cube Exchange. We talked about how the exchange works, why they made it, how it beats the competition and so much more!\n' +
+      '\n' +
+      'Watch the full interview here: https://www.youtube.com/watch?v=GcDhbJLHG-Q&t=58s',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_wave'
+  }
+];
--- src/views/wave/home/index.tsx
+++ src/views/wave/home/index.tsx
@@ -269,6 +269,19 @@ export const WaveHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 3</div>
               </Link>
+              <Link
+                href="/wave/drop4"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/-LrzYsu1Ma0-moekz7sFgSPJyBExLNYztdAcWmDQTck?ext=png"
+                    alt="drop 4 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
+              </Link>
             </div>
           </div>
         </div>
