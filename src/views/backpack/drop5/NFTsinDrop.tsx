737b93fda9a7e84faf5b714693d04d27dd876c96
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-09 08:30:45 +0100

    add drop5 backpack

---
+++ src/views/backpack/drop5/NFTsinDrop.tsx
@@ -0,0 +1,10 @@
+export const NFTsinDrop = [
+  {
+    name: 'Backpack Splash',
+    uri: 'https://arweave.net/yTkJ6nK8IC2AK6yx0_ucKgDUY_SEoumq4wj8eQUgKZg',
+    rarity: 'Common',
+    image: 'https://arweave.net/5nfl7uAKPHkdcqEROM8D_y4zHuKK9_TA9qFjxQQ7V0I?ext=gif',
+    description: 'Tyler Larronde from the Mad Lads Roster is a professional big wave surfer.',
+    magicEdenLink: 'https://drip.tensor.trade/trade/backpack_drip'
+  }
+];
--- src/views/backpack/home/index.tsx
+++ src/views/backpack/home/index.tsx
@@ -266,6 +266,19 @@ export const BackpackHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 4</div>
               </Link>
+              <Link
+                href="/backpack/drop5"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/5nfl7uAKPHkdcqEROM8D_y4zHuKK9_TA9qFjxQQ7V0I?ext=gif"
+                    alt="drop 5 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 5</div>
+              </Link>
             </div>
           </div>
         </div>
