bea5c7dd65f2603164caddf71c2c03cf1b1a7542
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-13 10:22:00 +0100

    add drop19 drop nation

---
+++ src/views/dropnation/drop19/NFTsinDrop.tsx
@@ -0,0 +1,26 @@
+export const NFTsinDrop = [
+  {
+    name: 'RUMBA',
+    uri: 'https://arweave.net/9XhtPCRa4bMpPk7sv-AnorKttq7GZMCnW3idf02cAXk',
+    rarity: 'Common',
+    image: 'https://arweave.net/bk23vUozPZvEQ4UdnFmmB2BN-79uNUPKqxypEBTdUfQ?ext=jpg',
+    description: "A bass-heavy and energetic Tech House anthem by Mike Epsse! 🛸 The perfect tune to get down to! 💃 RUMBA is the first song in Mike Epsse's second DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  },
+  {
+    name: 'MUEVE',
+    uri: 'https://arweave.net/ZF_3AFYoSzxHWNYS1KnzJYIYcuJ6uamc_4c3bW_9EgE',
+    rarity: 'Rare',
+    image: 'https://arweave.net/jn6pC9cOMbuOGrz86OpWNgaRSaolYaI9YVMfkbpDbHA?ext=jpg',
+    description: "A crowd-controlling Tech House banger by Mike Epsse & Vitamin THC! 🔥🕺 MUEVE is the second song in Mike Epsse's second DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  },
+  {
+    name: 'MALVADAO',
+    uri: 'https://arweave.net/PCAvdJKjnfjyMOqSxZoh9vSJWEZa8sLfCJubhvxlAjw',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/FF3kRvtMGpqvpvtS1jrKkl9PjFQEb_GedzWLPG2UKSk?ext=jpg',
+    description: "All aboard! This psychedelic Brazilian Tech House banger by Mike Epsse is leaving Earth!🛸🔥! MALVADAO is the third song in Mike Epsse's second DRiP on Drop Nation💧!",
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_dropnation'
+  }
+];
