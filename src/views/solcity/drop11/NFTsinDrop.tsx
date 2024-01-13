9880875011d03d6b7ddf5b6f1c82efe8aa086664
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-01-13 10:26:32 +0100

    add drop11 solcity

---
+++ src/views/solcity/drop11/NFTsinDrop.tsx
@@ -0,0 +1,25 @@
+export const NFTsinDrop = [
+  {
+    name: 'PixelDRiP',
+    uri: 'https://arweave.net/mRqAimm0Vu5DP7n9BLNJkVz5Oa_YlmwmH61_RjVx7C4',
+    rarity: 'Common',
+    image: 'https://arweave.net/8DZ1f2sLyEZjxBq5q__7dVsQPxcFUzRrTNlWXyMA3Ps?ext=png',
+    description: 'A Community Pixel Canvas created by SolCity in collaboration with SpriteBox, a growing platform with the mission to champion Pixel Art. This Canvas includes artworks by: @tevoeatacid @Dei_Septentrion @avinroart @bnuuy_land @NotoriousmanXTZ @canary_islander @Roberto_0z @aychhe @VoidsNatural @BiteCoinz @XO12XX & @thejomshoots',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Bestowed Gifts',
+    uri: 'https://arweave.net/s8ivuNj6VYuLzwmOqmYbcypOlrmBWy7XiFfZu7JeSiI',
+    rarity: 'Rare',
+    image: 'https://arweave.net/LZcdZcH6cHg-Vmc1mBheITo0jb9fwcrQndmW_apOsWI?ext=png',
+    description: 'Gifts Bestowed To Transcend Beyond What The Tears Can Reflect',
+    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  },
+  {
+    name: 'Beary Pops In',
+    uri: 'https://arweave.net/ooTwykcQ2J578JWzHvW1XEmQ5QM0TdHEjL3TRu5rNTQ',
+    rarity: 'Legendary',
+    image: 'https://arweave.net/Y4kUSDHCAz0-0J24ghr1kqXozLyW-NBzJ5D41HJP4sA?ext=gif',
+    description: 'There is magic in "the bear". An umbrella from a flying nanny finds its way to a pixelated character from the strange land of NFTS.',    magicEdenLink: 'https://www.tensor.trade/trade/drip_solcity'
+  }
+];
--- src/views/solcity/home/index.tsx
+++ src/views/solcity/home/index.tsx
@@ -344,6 +344,19 @@ export const SolcityHome: FC = ({}) => {
                 </div>
                 <div className="text-center font-bold mt-1 pb-1">Drop 10</div>
               </Link>
+              <Link
+                href="/solcity/drop11"
+                className="bg-[#000000] pt-1 rounded-xl border-2 border-[#FFFFFF] hover:border-[#14F195]"
+              >
+                <div className="flex justify-center">
+                  <img
+                    className=""
+                    src="https://arweave.net/Y4kUSDHCAz0-0J24ghr1kqXozLyW-NBzJ5D41HJP4sA?ext=gif"
+                    alt="drop 11 preview"
+                  ></img>
+                </div>
+                <div className="text-center font-bold mt-1 pb-1">Drop 11</div>
+              </Link>
             </div>
           </div>
         </div>
